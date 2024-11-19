25a08fad6695e649bd7897e93882873866ad915f
Rob McCary <45126540+Mccaryr@users.noreply.github.com>
2024-11-19 11:52:17 -0500

    added newly deployed project

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -5,11 +5,18 @@ import {useEffect, useState} from "react";
 import img1 from '../assets/kaiju-1.png'
 import img2 from '../assets/kaiju-2.png'
 import img3 from '../assets/kaiju-3.png'
+import img4 from '../assets/wisney-1.png'
+import img5 from '../assets/wisney-2.png'
+import img6 from '../assets/wisney-3.png'
 
 const Projects = () => {
     const [currentImageIndex, setCurrentImageIndex] = useState(0);
+    const [currentWisneyImageIndex, setCurrentWisneyImageIndex] = useState(0);
     const images = [img1, img2, img3]
+    const wisneyImages = [img4, img5, img6]
     const [isHovered, setIsHovered] = useState(false);
+    const [isWisneyHovered, setIsWisneyHovered] = useState(false);
+
 
 
     useEffect(() => {
@@ -24,6 +31,18 @@ const Projects = () => {
         return () => clearInterval(interval);
     }, [isHovered, images.length]);
 
+    useEffect(() => {
+        let wisneyInterval;
+        if (isWisneyHovered) {
+            wisneyInterval = setInterval(() => {
+                setCurrentWisneyImageIndex(prevIndex => (prevIndex + 1) % wisneyImages.length);
+            }, 1500);
+        } else {
+            setCurrentWisneyImageIndex(0);
+        }
+        return () => clearInterval(wisneyInterval);
+    }, [isWisneyHovered, wisneyImages.length]);
+
     return (
         <div id='recent projects' className='mt-5 py-5 px-[4rem]'>
             <motion.div variants={textVariant()}>
@@ -37,14 +56,25 @@ const Projects = () => {
                     to live demos. Enjoy!
                 </motion.p>
             </div>
-            <div className="w-full h-1/2 flex justify-center pt-5">
+            <div className="w-full h-1/2 flex justify-center pt-5 gap-10 flex-wrap md:flex-nowrap">
                 <img
                     src={images[currentImageIndex]}
                     alt={'Kaiju Project'}
                     onMouseEnter={() => setIsHovered(true)}
                     onMouseLeave={() => setIsHovered(false)}
-                    className='w-1/2 cursor-pointer'
+                    className='md:w-1/2 cursor-pointer'
                     onClick={() => window.open("https://kaiju-mke1.onrender.com/", '_blank')}
+                />
+                <img
+                    src={wisneyImages[currentWisneyImageIndex]}
+                    alt={'Wisney Project'}
+                    onMouseEnter={() => {
+                        setIsWisneyHovered(true)
+                        setIsHovered(false)
+                    }}
+                    onMouseLeave={() => setIsWisneyHovered(false)}
+                    className='md:w-1/2 cursor-pointer'
+                    onClick={() => window.open("https://dalt-wisney-ecommerce.onrender.com/", '_blank')}
                 />
             </div>
         </div>
