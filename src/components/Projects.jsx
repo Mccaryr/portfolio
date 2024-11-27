645360e073e4675b871c08b222c1cb6bf6417ccd
Rob McCary <45126540+Mccaryr@users.noreply.github.com>
2024-11-27 17:13:23 -0500

    updated portfolio with new project

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -8,14 +8,19 @@ import img3 from '../assets/kaiju-3.png'
 import img4 from '../assets/wisney-1.png'
 import img5 from '../assets/wisney-2.png'
 import img6 from '../assets/wisney-3.png'
+import img7 from '../assets/recipe-app/rfd-1.png'
+import img8 from '../assets/recipe-app/rfd-2.png'
 
 const Projects = () => {
     const [currentImageIndex, setCurrentImageIndex] = useState(0);
     const [currentWisneyImageIndex, setCurrentWisneyImageIndex] = useState(0);
+    const [currentRFDImageIndex, setCurrentRFDImageIndex] = useState(0);
     const images = [img1, img2, img3]
     const wisneyImages = [img4, img5, img6]
+    const rfdImages = [img7, img8]
     const [isHovered, setIsHovered] = useState(false);
     const [isWisneyHovered, setIsWisneyHovered] = useState(false);
+    const [isRFDHovered, setIsRFDHovered] = useState(false);
 
 
 
@@ -43,6 +48,18 @@ const Projects = () => {
         return () => clearInterval(wisneyInterval);
     }, [isWisneyHovered, wisneyImages.length]);
 
+    useEffect(() => {
+        let rfdInterval;
+        if (isRFDHovered) {
+            rfdInterval = setInterval(() => {
+                setCurrentRFDImageIndex(prevIndex => (prevIndex + 1) % rfdImages.length);
+            }, 1500);
+        } else {
+            setCurrentRFDImageIndex(0);
+        }
+        return () => clearInterval(rfdInterval);
+    }, [isRFDHovered, wisneyImages.length]);
+
     return (
         <div id='recent projects' className='mt-5 py-5 px-[4rem]'>
             <motion.div variants={textVariant()}>
@@ -52,30 +69,50 @@ const Projects = () => {
             <div className="w-full flex">
                 <motion.p variants={fadeIn("", "", 0.1, 1)}
                           className="mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]">
-                    Following projects showcases my skills with real world examples of my work. Each project is linked
-                    to live demos. Enjoy!
+                    Following projects showcases my skills with real world examples of my work. 
+                    Click on each project to view live demos. Enjoy!
                 </motion.p>
             </div>
-            <div className="w-full h-1/2 flex justify-center pt-5 gap-10 flex-wrap md:flex-nowrap">
-                <img
-                    src={images[currentImageIndex]}
-                    alt={'Kaiju Project'}
-                    onMouseEnter={() => setIsHovered(true)}
-                    onMouseLeave={() => setIsHovered(false)}
-                    className='md:w-1/2 cursor-pointer'
-                    onClick={() => window.open("https://kaiju-mke1.onrender.com/", '_blank')}
-                />
-                <img
-                    src={wisneyImages[currentWisneyImageIndex]}
-                    alt={'Wisney Project'}
-                    onMouseEnter={() => {
-                        setIsWisneyHovered(true)
-                        setIsHovered(false)
-                    }}
-                    onMouseLeave={() => setIsWisneyHovered(false)}
-                    className='md:w-1/2 cursor-pointer'
-                    onClick={() => window.open("https://dalt-wisney-ecommerce.onrender.com/", '_blank')}
-                />
+            <div className="w-full h-1/2 flex justify-center pt-5 gap-10 flex-wrap">
+                <div className="flex flex-col justify-center items-center">
+                    <a href={'https://github.com/Mccaryr/kaiju'} target="_blank" rel="noopener noreferrer">Github
+                        repo</a>
+                    <img
+                        src={images[currentImageIndex]}
+                        alt={'Kaiju Project'}
+                        onMouseEnter={() => setIsHovered(true)}
+                        onMouseLeave={() => setIsHovered(false)}
+                        className='md:w-3/4 cursor-pointer'
+                        onClick={() => window.open("https://kaiju-mke1.onrender.com/", '_blank')}
+                    />
+                </div>
+                <div className="flex flex-col justify-center items-center">
+                    <a href={'https://github.com/Mccaryr/dalt-wisney-ecommerce'} target="_blank"
+                       rel="noopener noreferrer">Github repo</a>
+                    <img
+                        src={wisneyImages[currentWisneyImageIndex]}
+                        alt={'Wisney Project'}
+                        onMouseEnter={() => {
+                            setIsWisneyHovered(true)
+                            setIsHovered(false)
+                        }}
+                        onMouseLeave={() => setIsWisneyHovered(false)}
+                        className='md:w-3/4 cursor-pointer'
+                        onClick={() => window.open("https://dalt-wisney-ecommerce.onrender.com/", '_blank')}
+                    />
+                </div>
+                <div className="flex flex-col justify-center items-center">
+                    <img
+                        src={rfdImages[currentRFDImageIndex]}
+                        alt={'Recipe for Disaster Project'}
+                        onMouseEnter={() => {
+                            setIsRFDHovered(true)
+                        }}
+                        onMouseLeave={() => setIsRFDHovered(false)}
+                        className='md:w-3/4 cursor-pointer'
+                        onClick={() => window.open("https://recipe-for-disaster-liart.vercel.app/", '_blank')}
+                    />
+                </div>
             </div>
         </div>
     )
